cb5da0309613f0f3b45219434af37c55b3216773
Kyungyun Kim <kyknn79@naver.com>
2020-08-31 18:21:05 +0900

    share edit

--- V1.5/js/index.js
+++ V1.5/js/index.js
@@ -4,8 +4,8 @@ Kakao.Link.createDefaultButton({
   container: "#kakao-link-btn",
   objectType: "feed",
   content: {
-    title: "디저트 사진",
-    description: "아메리카노, 빵, 케익",
+    title: "ForestSnap",
+    description: "궁금한 꽃, 버섯, 열매, 풀 사진을 찍어 찾아보세요",
     imageUrl:
       "http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
     link: {
@@ -16,12 +16,6 @@ Kakao.Link.createDefaultButton({
   buttons: [
     {
       title: "웹으로 이동",
-      link: {
-        mobileWebUrl: "https://developers.kakao.com",
-      },
-    },
-    {
-      title: "앱으로 이동",
       link: {
         mobileWebUrl: "https://developers.kakao.com",
       },
